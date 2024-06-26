9c6f0afae15a47631c87f0bd0176bdef346abb54
nickkpiccoli <piccolibeatz@gmail.com>
2024-06-26 18:01:32 +0200

    New api in users
    Added api to manage customer and agents orders

--- routes/users.js
+++ routes/users.js
@@ -21,11 +21,17 @@ var pool = new Pool({
     connectionString: conString
 });
 
+var{ MainPool } = require('pg');
+var mainConString = "postgres://postgres:studio@localhost/organization"
+
+var mainPool = new Pool({
+    connectionString: mainConString
+});
+
 //endpoint for user login, require an user id and the password for that user
 router.post("/login", (req,res) => {
     const username = req.body.username;
     const userPassword = req.body.password;
-
 
     pool.connect(function(err, client, done) {
         if (err) {
@@ -81,21 +87,158 @@ router.post("/login", (req,res) => {
 
 })
 
-router.get("/getorders", (req,res) => {
+//return for the customer who make the request the corresponding orders with the agent responsible for the orders
+router.get("/getCustomerOrders", (req,res) => {
     const requestingUser = verifyToken(req,res);
     if (Object.keys(requestingUser).length !== 0){
-
+        const userCode = requestingUser.userCode;
+        if(requestingUser.userRole !== "customer"){
+            res.status(401).json({error: "User is not a customer"})
+        }
+        else {
+            mainPool.connect(function (err, client, done) {
+                if (err) {
+                    console.error('error fetching client from pool', err);
+                    res.status(500).json({error: 'Database connection error'});
+                }
+                //recupero degli ordini dell'utente
+                client.query("SELECT * from orders o join agents a on o.agent_code = a.agent_code where o.cust_code=$1", [userCode], function (err, result) {
+                    done();
+                    if (err) {
+                        console.error('error running query', err);
+                        res.status(500).json({error: 'Query to database failed'});
+                    }
+                    if (result.rows.length > 0) {
+                        console.log(result)
+                        res.json({orders: result.rows})
+                    } else {
+                        return res.status(404).json({error: 'User not found'});
+                    }
+                });
+            });
+        }
     }
     else{
         return res.status(401).json({ error: 'Unauthorized User' });
     }
 })
 
-//mock function to test verify token
-router.get("/prova", (req,res) => {
-    console.log(verifyToken(req,res))
-    if (Object.keys(verifyToken(req,res)).length !== 0){
-        res.send("andato");
+//return for the agent who make the request the corresponding orders for whose is responsible with the customer of the order
+router.get("/getAgentOrders", (req,res) => {
+    const requestingUser = verifyToken(req,res);
+    if (Object.keys(requestingUser).length !== 0){
+        const userCode = requestingUser.userCode;
+        console.log(userCode)
+        if(requestingUser.userRole !== "agent"){
+            res.status(401).json({error: "User is not an agent"})
+        }
+        else {
+            mainPool.connect(function (err, client, done) {
+                if (err) {
+                    console.error('error fetching client from pool', err);
+                    res.status(500).json({error: 'Database connection error'});
+                }
+                //recupero degli ordini dell'utente
+                client.query("SELECT * from orders o join customer c on o.cust_code = c.cust_code where o.agent_code=$1", [userCode], function (err, result) {
+                    done();
+                    if (err) {
+                        console.error('error running query', err);
+                        res.status(500).json({error: 'Query to database failed'});
+                    }
+                    if (result.rows.length > 0) {
+                        console.log(result)
+                        res.json({orders: result.rows})
+                    } else {
+                        return res.status(404).json({error: 'User not found'});
+                    }
+                });
+            });
+        }
+    }
+    else{
+        return res.status(401).json({ error: 'Unauthorized User' });
+    }
+})
+
+//TODO CAPIRE COME STRUTTURARE LA MODIFICA
+router.post("/modifyAgentOrder", (req,res) => {
+
+})
+
+//delete an order held by the agent, use order id to delete the record
+router.post("/deleteAgentOrder", (req,res) => {
+    const order_num = req.body.order_num;
+    const requestingUser = verifyToken(req,res);
+    if (Object.keys(requestingUser).length !== 0){
+        const userCode = requestingUser.userCode;
+        if(requestingUser.userRole !== "agent"){
+            res.status(401).json({error: "User is not an agent"})
+        }
+        else {
+            mainPool.connect(function (err, client, done) {
+                if (err) {
+                    console.error('error fetching client from pool', err);
+                    res.status(500).json({error: 'Database connection error'});
+                }
+                //recupero degli ordini dell'utente
+                client.query("delete from orders where ord_num=$1 and agent_code=$2", [order_num, userCode], function (err, result) {
+                    done();
+                    if (err) {
+                        console.error('error running query', err);
+                        res.status(500).json({error: 'Query to database failed'});
+                    }
+                    if (result.rowCount === 1 && result.command === "DELETE"){
+                        res.status(200).json("Order deleted successfully")
+                    }
+                    else{
+                        res.status(404).json({error: 'Order not found'});
+                    }
+                    console.log(result);
+                    /*else {
+                        return res.status(200).json({error: 'Order deleted successfully'});
+                    }*/
+                });
+            });
+        }
+    }
+    else{
+        return res.status(401).json({ error: 'Unauthorized User' });
+    }
+})
+
+router.post("/addAgentOrder", (req,res) => {
+    const newOrder = req.body.order;
+    const requestingUser = verifyToken(req,res);
+    if (Object.keys(requestingUser).length !== 0){
+        const userCode = requestingUser.userCode;
+        if(requestingUser.userRole !== "agent"){
+            res.status(401).json({error: "User is not an agent"})
+        }
+        else {
+            mainPool.connect(function (err, client, done) {
+                if (err) {
+                    console.error('error fetching client from pool', err);
+                    res.status(500).json({error: 'Database connection error'});
+                }
+                //recupero degli ordini dell'utente
+                client.query("insert into orders (ord_num, ord_amount, advance_amount, ord_date, cust_code, agent_code, ord_description) values ($1,$2,$3,$4,$5,$6,$7)", [newOrder.ord_num, newOrder.ord_amount, newOrder.advance_amount, newOrder.ord_date, newOrder.cust_code, newOrder.agent_code, newOrder.ord_description], function (err, result) {
+                    done();
+                    if (err) {
+                        console.error('error running query', err);
+                        res.status(500).json({error: 'Query to database failed'});
+                    }
+                    console.log(result)
+                    if (result.rowCount === 1 && result.command === "INSERT"){
+                        res.status(200).json("Order inserted successfully")
+                    }
+                    else{
+                        //TODO CAPIRE CHE CODICE METTERE
+                        res.status(404).json({error: 'Order not found'});
+                    }
+                    console.log(result);
+                });
+            });
+        }
     }
     else{
         return res.status(401).json({ error: 'Unauthorized User' });
