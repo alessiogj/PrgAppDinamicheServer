2a154e97b52639de400bd456ed226577e6fb897a
nickkpiccoli <piccolibeatz@gmail.com>
2024-06-30 15:43:12 +0200

    refactor and new endpoints
    Refactor of branches of old endpoint added dirigent endponts

--- routes/users.js
+++ routes/users.js
@@ -35,42 +35,45 @@ router.post('/login', (req, res) => {
             console.error('Error fetching client from pool', err);
             return res.status(500).json({ error: 'Database connection error' });
         }
+        else{
+            client.query("SELECT user_code, password, user_role FROM users WHERE username = $1", [username], (err, result) => {
+                done();
+                if (err) {
+                    console.error('Error running query', err);
+                    return res.status(500).json({ error: 'Query to database failed' });
+                }
+                else{
+                    if (result.rows.length > 0) {
+                        const { user_code, password: storedHash, user_role } = result.rows[0];
+                        bcrypt.compare(password, storedHash, (err, isMatch) => {
+                            if (err) {
+                                console.error('Bcrypt error:', err);
+                                return res.status(500).json({ error: 'Error verifying password' });
+                            }
+                            else {
+                                if (isMatch) {
+                                    const jwtSecretKey = jwtSecretKeyConfig;
+                                    const tokenData = {
+                                        userCode: user_code,
+                                        userRole: user_role,
+                                        time: Date.now()
+                                    };
 
-        client.query("SELECT user_code, password, user_role FROM users WHERE username = $1", [username], (err, result) => {
-            done();
-            if (err) {
-                console.error('Error running query', err);
-                return res.status(500).json({ error: 'Query to database failed' });
-            }
+                                    const token = jwt.sign(tokenData, jwtSecretKey, {expiresIn: '24h'});
 
-            if (result.rows.length > 0) {
-                const { user_code, password: storedHash, user_role } = result.rows[0];
-
-                bcrypt.compare(password, storedHash, (err, isMatch) => {
-                    if (err) {
-                        console.error('Bcrypt error:', err);
-                        return res.status(500).json({ error: 'Error verifying password' });
+                                    res.status(200).json({token});
+                                } else {
+                                    res.status(401).json({error: 'Invalid password'});
+                                }
+                            }
+                        });
                     }
-
-                    if (isMatch) {
-                        const jwtSecretKey = jwtSecretKeyConfig;
-                        const tokenData = {
-                            userCode: user_code,
-                            userRole: user_role,
-                            time: Date.now()
-                        };
-
-                        const token = jwt.sign(tokenData, jwtSecretKey, { expiresIn: '24h' });
-
-                        res.status(200).json({ token });
-                    } else {
-                        res.status(401).json({ error: 'Invalid password' });
+                    else {
+                        res.status(404).json({ error: 'User not found' });
                     }
-                });
-            } else {
-                res.status(404).json({ error: 'User not found' });
-            }
-        });
+                }
+            });
+        }
     });
 });
 
@@ -100,6 +103,7 @@ router.get('/getCustomerOrders', verifyToken, async (req, res) => {
                 console.error('error fetching client from pool', err);
                 res.status(500).json({error: 'Database connection error'});
             }
+            else{
             //recupero degli ordini dell'utente
             client.query("SELECT * FROM orders o JOIN agents a ON o.agent_code = a.agent_code WHERE o.cust_code = $1",
                 [req.user.userCode],
@@ -111,6 +115,7 @@ router.get('/getCustomerOrders', verifyToken, async (req, res) => {
                     }
                     res.json({orders: result.rows});
                 });
+            }
         }
     )});
 
@@ -125,8 +130,9 @@ router.get('/getAgentOrders', verifyToken, async (req, res) => {
                 console.error('error fetching client from pool', err);
                 res.status(500).json({error: 'Database connection error'});
             }
+            else{
             //recupero degli ordini dell'utente
-            client.query("SELECT * FROM orders o  customer c ON o.cust_code = c.cust_code WHERE o.agent_code = $1",
+            client.query("SELECT * FROM orders o JOIN customer c ON o.cust_code = c.cust_code WHERE o.agent_code = $1",
                 [req.user.userCode],
                 function (err, result) {
                     done();
@@ -138,6 +144,7 @@ router.get('/getAgentOrders', verifyToken, async (req, res) => {
                         res.json({orders: result.rows});
                     }
                 });
+            }
         }
     )});
 
@@ -163,7 +170,7 @@ router.put('/modifyAgentOrder', verifyToken, async (req,res) => {
                     if (result.rowCount > 0) {
                         res.status(200).json("order modified successfully")
                     } else {
-                        return res.status(404).json({error: 'User not found'});
+                        return res.status(404).json({error: 'Order not found'});
                     }
                 }
             });
@@ -180,6 +187,7 @@ router.delete('/deleteAgentOrder', verifyToken, async (req, res) => {
             console.error('error fetching client from pool', err);
             res.status(500).json({error: 'Database connection error'});
         }
+        else{
         client.query("DELETE FROM orders WHERE ord_num = $1;",
             [req.body.ord_num],
             function (err, result) {
@@ -193,6 +201,7 @@ router.delete('/deleteAgentOrder', verifyToken, async (req, res) => {
                     res.status(404).json({error: 'Order not found'});
                 }
             });
+        }
     });
 });
 
@@ -208,6 +217,7 @@ router.post('/addAgentOrder', verifyToken, async (req, res) => {
             console.error('error fetching client from pool', err);
             res.status(500).json({error: 'Database connection error'});
         }
+        else{
         client.query("INSERT INTO orders (ord_num, ord_amount, advance_amount, ord_date, cust_code, agent_code, ord_description) VALUES ($1, $2, $3, $4, $5, $6, $7);",
             [newOrder.ord_num, newOrder.ord_amount, newOrder.advance_amount, newOrder.ord_date, newOrder.cust_code, newOrder.agent_code, newOrder.ord_description],
             function (err, result) {
@@ -219,7 +229,88 @@ router.post('/addAgentOrder', verifyToken, async (req, res) => {
                     res.status(200).json({message: "Order added successfully"});
                 }
             });
+        }
     });
 });
+
+router.get('/getAvailableCustomers', verifyToken, async (req,res) => {
+    if (req.user.userRole !== 'agent') {
+        return res.status(401).json({ error: 'User is not an agent' });
+    }
+    await poolOrganization.connect(function (err, client, done) {
+        if (err) {
+            console.error('error fetching client from pool', err);
+            res.status(500).json({error: 'Database connection error'});
+        }
+        else {
+            client.query("select cust_code from customer",function (err, result) {
+                    done();
+                    if (err) {
+                        console.error('error running query', err);
+                        res.status(500).json({error: 'Query to database failed'});
+                    }
+                    else {
+                        res.json({customers: result.rows});
+                    }
+                });
+        }
+    });
+
+})
+
+router.get('/getDirigentOrders', verifyToken, async (req,res) => {
+    if (req.user.userRole !== 'dirigent') {
+        return res.status(401).json({ error: 'User is not a dirigent' });
+    }
+    await poolOrganization.connect(function (err, client, done) {
+        if (err) {
+            console.error('error fetching client from pool', err);
+            res.status(500).json({error: 'Database connection error'});
+        }
+        else {
+            client.query("select * from orders o join customer c on o.cust_code = c.cust_code join agents a on o.agent_code = a.agent_code",function (err, result) {
+                done();
+                if (err) {
+                    console.error('error running query', err);
+                    res.status(500).json({error: 'Query to database failed'});
+                }
+                else {
+                    res.json({orders: result.rows});
+                }
+            });
+        }
+    });
+
+})
+
+router.put('/modifyDirigentOrder', verifyToken, async (req,res) => {
+    const updatedOrder = req.body.modifiedOrder;
+    if (req.user.userRole !== 'dirigent') {
+        return res.status(401).json({ error: 'User is not a dirigent' });
+    }
+    await poolOrganization.connect(function (err, client, done) {
+        if (err) {
+            console.error('error fetching client from pool', err);
+            res.status(500).json({error: 'Database connection error'});
+        }
+        client.query("UPDATE orders SET ord_num = $1, ord_amount = $2, advance_amount = $3, ord_date = $4, cust_code = $5, agent_code = $6, ord_description = $7 WHERE ord_num = $1;",
+            [updatedOrder.ord_num, updatedOrder.ord_amount, updatedOrder.advance_amount, updatedOrder.ord_date, updatedOrder.cust_code, updatedOrder.agent_code, updatedOrder.ord_description],
+            function (err, result) {
+                done();
+                if (err) {
+                    console.error('error running query', err);
+                    res.status(500).json({error: 'Query to database failed'});
+                }
+                else {
+                    if (result.rowCount > 0) {
+                        res.status(200).json("order modified successfully")
+                    } else {
+                        return res.status(404).json({error: 'Order not found'});
+                    }
+                }
+            });
+    });
+})
+
 
 module.exports = router;
